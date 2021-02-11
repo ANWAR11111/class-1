0184ad1c21fe02adae6dad5e8300038ca1d09386
ANWAR11111 <50073733+ANWAR11111@users.noreply.github.com>
2021-02-11 19:29:24 +0500

    Add files via upload

---
+++ 11th class/app.js
@@ -0,0 +1,104 @@
+/*var arr = ["cat","dog","tiger","lion"]
+console.log(arr[2])
+
+
+
+/*var a;
+for(initialization; condition; increment)
+docoment.write(a)*/
+
+/*var x;
+for(x=1; x<=10 ; x+2){
+    console.log(x)
+}
+
+for(var i = 1; i<=10; i++){
+    decodeURIComponent.write("2" + "x" + i + "=" + 2*1 + "<br>")
+}
+
+var names = ["muazamil","kamran","basit"];
+
+for(var i = 0; i< names.length; i++){
+    console.log(name[i])
+}
+
+var cities = ["kara","multan","islamabd"];
+
+for(var i = 0; i< cities.length; i++){
+    if (cities[i] ==="kara"){
+        alert(cities[i] + "is the cleanest city of pakistan")
+    }
+}
+
+
+
+
+for(var i = 0; i <10; i ++){
+    if(i===5){
+        break;
+    }
+    console.log("hello")
+}
+
+for(var i=0; i < 5; i++){
+    for(var j = 0; j < 3; j++){
+        docoment.write(j + "Pakistan Zindabad")
+    }
+}
+
+
+var firstNames = ["ghaus","basit","fahad"]
+var lastNames = ["ahmed","khan"]
+
+for(var i = 0; i < firstNamnes.length; i++){
+    for(var x = 0; x < lastNmaes.length; x++){
+        Console.log(firstNames[i] + " " lastNmaes[x])
+    }
+}*/
+
+/*var word = prompt("Enter your word");
+var check = "";
+
+for(var i = word.length -1; i >= 0; i--){
+    check += word[i]
+
+}
+if(word === check){{
+    console.log(word +" " +"is palingram wor")
+}}*/
+
+ /*var name = "MAUZAMMIL";
+ name = name.toLowerCase();
+ console.log(name)*/
+
+
+
+
+ /*var name = "MAUZAMMIL";
+ console.log(name.indexOf("A"))*/
+
+
+/*
+var val1 = prompt('enter value')
+var val2 = prompt('enter value')
+var sign = prompt('enter value')
+
+console.log(val1+sign+val2)
+if (sign === '+'){
+    alert(val1+sign+val2)
+}else if(sign === '-'){
+    alert(val1-val2)
+}else if(sign === '*'){
+    alert(val1-val2)
+}else if(sign === '/'){
+    alert(val1-val2)
+}*/
+
+
+
+var equ = prompt('Enter your equation');
+var num = equ.length
+
+for(var i = 0; i<num; i++){
+    if(equ.slice(i,i+1)) ==='+'||
+}
